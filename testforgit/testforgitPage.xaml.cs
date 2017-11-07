6b015912d5208f39cf3678632f72a1c62fda2acb
Arvy Bugtong <arvybugtong@gmail.com>
2017-11-07 10:17:58 +0800

    First commit

---
+++ testforgit/testforgitPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace testforgit
+{
+    public partial class testforgitPage : ContentPage
+    {
+        public testforgitPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
